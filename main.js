bd4938e6f98b7f593c9a6be05c7bcdc60d8a932b
Thibault Guilhem <thibault.guilhem@gmail.com>
2025-04-06 18:29:37 +0200

    update cv.html and main.js

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  const fullNameValue = "Jean Albert";
-  const emailValue = "jeanalbert";
+  const firstName = "Jean";
+  const lastName = "Albert";
   const phoneValue = "06 24 55 00 17";
   const locationValue = "Paris";
-  const githubValue = "JeanA";
-  const linkedInValue = "jean-a";
+  const fullNameValue = `${firstName} ${lastName}`;
+  const emailValue = firstName.toLowerCase() + lastName.toLowerCase();
+  const githubValue = firstName + lastName[0];
+  const linkedInValue = `${firstName.toLowerCase()}-${lastName[0].toLowerCase()}`;
 
   const fullNames = document.querySelectorAll(".fullname-span");
-  const email = document.getElementById("email");
+  const email = document.querySelectorAll(".email-span");
   const phone = document.getElementById("phone");
   const location = document.getElementById("location");
   const github = document.getElementById("github");
@@ -16,8 +18,10 @@ document.addEventListener("DOMContentLoaded", async () => {
   fullNames.forEach((fullName) => {
     fullName.textContent = fullNameValue;
   });
+  email.forEach((email) => {
+    email.textContent = emailValue;
+  });
 
-  email.textContent = emailValue;
   phone.textContent = phoneValue;
   location.textContent = locationValue;
   github.textContent = githubValue;
